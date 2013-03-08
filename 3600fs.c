e9f5bab6531b31311e4bb9a3fac9396feb2cc5e0
Kevin L <klanger@agentx.ccs.neu.edu>
2013-03-08 01:14:54 -0500

    Supports extended length files between 0-4096 bytes. Segfault on read

--- 3600fs.c
+++ 3600fs.c
@@ -336,7 +336,6 @@ static int vfs_read(const char *path, char *buf, size_t size, off_t offset,
 static int vfs_write(const char *path, const char *buf, size_t size,
                      off_t offset, struct fuse_file_info *fi)
 {
-
   /* 3600: NOTE THAT IF THE OFFSET+SIZE GOES OFF THE END OF THE FILE, YOU
            MAY HAVE TO EXTEND THE FILE (ALLOCATE MORE BLOCKS TO IT). */
 	fprintf(stderr,"vfs_write called on %s for size %d and offset %d\n",path,size,offset);
@@ -370,8 +369,7 @@ static int vfs_write(const char *path, const char *buf, size_t size,
     fat_count = 0; 
     fats = (size+offset)/BLOCKSIZE + 1;
     int cpy_size = 0;
-    while (fat_count < fats) { 
-        fprintf(stderr,"Fatcount: %d\nData block: %d\n",fat_count,data_block_num);    
+    while (fat_count < fats) {   
 	    int byte_offset = offset;
 	    dread(vcBlock->db_start + data_block_num, data_block);	   
         if (fat_count == fats-1) {
@@ -388,16 +386,16 @@ static int vfs_write(const char *path, const char *buf, size_t size,
         fat_count++; 	    
         fatEntry->used = 1;
 	    fatEntry->eof = 1;
-        if ( fat_count >= fats ) {
-            data_block_num = -1;
-        } else {
-            data_block_num = find_free_block();
-        }
-        fatEntry->next =data_block;
+        fatEntry->next =data_block_num;
         memcpy(data_block + fatent_block_offset*sizeof( fatent ), fatEntry, sizeof(fatent));
 	    dwrite(vcBlock->fat_start + fatent_block_num, data_block);
         memset(fatEntry,0,sizeof(fatEntry));
-        memset(data_block,0,sizeof(data_block));          
+        memset(data_block,0,sizeof(data_block));    
+        if ( fat_count > fats ) {
+            data_block_num = -1;
+        } else {
+            data_block_num = find_free_block();
+        }      
     }     
 	dwrite(block, dirEntry);
     free(dirEntry);
